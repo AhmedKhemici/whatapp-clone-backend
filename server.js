0dc0b8f54b7c374bc9ef164096728fa82e388463
Ahmed Khemici <ahmed.khemici@yib.io>
2023-10-24 21:55:11 +0100

    so far so good ?

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //  importig
 import express from "express";
+import mongoose from "mongoose";
 
 //  app config
 const app = express();
@@ -8,11 +9,19 @@ const port = process.env.PORT || 9000
 //  middleware
 
 //  DB config
+const connection_url = 'mongodb://root:example@127.0.0.1:27017/whatsapp'
+
+
+mongoose.connect(connection_url,{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 //  ?????
 
 //  api routes
 app.get("/",(req,res) => res.status(200).send("hello world"));
-s
+
 //  listen
 app.listen(port,()=>console.log(`Listening on localhost:${port}`));
