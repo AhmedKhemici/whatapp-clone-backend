db237a13786d6dcf68ad69d7ad9f29831cb29728
Ahmed Khemici <ahmed.khemici@yib.io>
2023-10-25 18:07:25 +0100

    new beginigs

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 //  importig
 import express from "express";
 import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+
 
 //  app config
 const app = express();
 const port = process.env.PORT || 9000
 
 //  middleware
+app.use(express.json());
 
 //  DB config
-const connection_url = 'mongodb://root:example@127.0.0.1:27017/'
+const connection_url = 'mongodb://localhost:27017/whatsapp'
 
 
 mongoose.connect(connection_url,{
@@ -21,6 +24,25 @@ mongoose.connect(connection_url,{
 
 //  api routes
 app.get("/",(req,res) => res.status(200).send("hello world"));
+
+app.post('/api/v1/messages/new', (req, res) => {
+    const dbMessage = req.body;
+    // Messages.create(dbMessage, (err, data) => {
+    //     if (err) {
+    //         res.status(500).send(err)
+    //     } else {
+    //         res.status(201).send(`new message create: \n ${data}`)
+    //     }
+    // })
+    const Message = new Messages(dbMessage);
+    Message.save().then(result => {
+        console.log('Created Message');
+        res.status(201).send(`new message create: \n ${result}`)
+    }).catch(err => {
+        console.log(err);
+        res.status(500).send(err)
+    });
+})
 
 //  listen
 app.listen(port,()=>console.log(`Listening on localhost:${port}`));
